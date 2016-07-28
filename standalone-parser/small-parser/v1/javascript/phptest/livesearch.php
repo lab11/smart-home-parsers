aae00430b852cb6ed6c5cb8e0e439fc27fee16c1
mclarkk <mclarkk@umich.edu>
2016-07-28 01:48:50 -0400

    figured out how to return records from a table when typing

--- standalone-parser/small-parser/v1/javascript/phptest/livesearch.php
+++ standalone-parser/small-parser/v1/javascript/phptest/livesearch.php
@@ -1,4 +1,27 @@
 <?php
+include "database-credentials.php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT Literal FROM VERB";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["Literal"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+/*
 $xmlDoc=new DOMDocument();
 $xmlDoc->load("links.xml");
 
@@ -6,6 +29,7 @@ $x=$xmlDoc->getElementsByTagName('link');
 
 //get the q parameter from URL
 $q=$_GET["q"];
+
 
 //lookup all links from the xml file if length of q>0
 if (strlen($q)>0) {
@@ -15,7 +39,8 @@ if (strlen($q)>0) {
     $z=$x->item($i)->getElementsByTagName('url');
     if ($y->item(0)->nodeType==1) {
       //find a link matching the search text
-      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+      //if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+      if (strtolower(substr($y->item(0)->childNodes->item(0)->nodeValue,0,strlen($q))) === strtolower($q)) {
         if ($hint=="") {
           $hint="<a href='" . 
           $z->item(0)->childNodes->item(0)->nodeValue . 
@@ -39,7 +64,7 @@ if ($hint=="") {
 } else {
   $response=$hint;
 }
-
+*/
 //output the response
-echo $response;
+//echo $response;
 ?>
