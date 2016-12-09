4da31b99f217416df5e4f9df78ae1006879c97f2
mclarkk <mclarkk@umich.edu>
2016-12-09 17:01:29 -0500

    fixed details in the update_database script to make it actually connect and then added it to the generate_grammar script

--- standalone-parser/small-parser/v1/javascript/generated-parser/generate_grammar.sh
+++ standalone-parser/small-parser/v1/javascript/generated-parser/generate_grammar.sh
@@ -3,3 +3,5 @@ alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSP
 alias grun='java org.antlr.v4.gui.TestRig'
 
 antlr4 -Dlanguage=JavaScript Nearby.g4
+
+python update_database.py
--- standalone-parser/small-parser/v1/javascript/generated-parser/update_database.py
+++ standalone-parser/small-parser/v1/javascript/generated-parser/update_database.py
@@ -4,20 +4,20 @@
 
 import re
 import pymysql.cursors
-import database_credentials
+import database_credentials as config
 
 special_rules = ["INTEGER"]
 
 def main():
     lines = get_lines()
     lexer_rules = get_lexer_rules(lines)
-    delete_database(dbname)
-    create_database(dbname)
-    build_tables_automatically(dbname, lexer_rules)
+    delete_database(config.dbname)
+    create_database(config.dbname)
+    build_tables_automatically(lexer_rules)
     build_tables_from_special_rules()
 
 def delete_database(dbname):
-    db = connect()
+    db = connect_mysql()
     try:
         cursor = db.cursor()
         cursor.execute("DROP DATABASE IF EXISTS " + dbname)
@@ -26,7 +26,7 @@ def delete_database(dbname):
         db.close()
 
 def create_database(dbname):
-    db = connect()
+    db = connect_mysql()
     try:
         cursor = db.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS " + dbname)
@@ -35,7 +35,7 @@ def create_database(dbname):
         db.close()
 
 def build_tables_automatically(lexer_rules):
-    db = connect()
+    db = connect_tokens_db()
     try:
         with db.cursor() as cursor:
             for lexer_rule in lexer_rules:
@@ -79,12 +79,20 @@ def get_lexer_rules(lines):
         rules[rule_name] = tokens
     return rules
 
-def connect():
+def connect_mysql():
     # Connect to the database
     connection = pymysql.connect(host='localhost',
-                                 user=username,
-                                 password=password,
-                                 db=dbname,
+                                 user=config.username,
+                                 password=config.password,
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+def connect_tokens_db():
+    # Connect to the database
+    connection = pymysql.connect(host='localhost',
+                                 user=config.username,
+                                 password=config.password,
+                                 db=config.dbname,
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
 
