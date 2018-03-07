66928c97c5c5622ee1fe3b0e338f1dc9c8999979
Meghan <meghan.leah.clark@gmail.com>
2018-03-06 16:37:38 -0800

    it generates a language! Now without passwords...

--- standalone-parser/small-parser/v1/javascript/generate_language.js
+++ standalone-parser/small-parser/v1/javascript/generate_language.js
@@ -1,12 +1,13 @@
 var mysql = require('mysql');
-var trans = require('./js/translator.js');
+var trans = require('js/translator');
+var dbconfig = require('secrets/database-credentials');
 
 var language = new Array();
 var partial_strs = new Array("");
 var parse_succeeded = null;
 var next_sentences = null;
 
-var ErrorListener = require('antlr4/error/ErrorListener.js').ErrorListener;
+var ErrorListener = require('antlr4/error/ErrorListener').ErrorListener;
 function TestGrammarErrorListener() {
     ErrorListener.call(this);
     this.partialApplication = null;
@@ -22,16 +23,16 @@ var testParser;
 TestGrammarErrorListener.prototype.syntaxError = function(recognizer, offendingSymbol, line, column, msg, e) {
     this.errors.push(arguments);
     parse_succeeded = false;
-    console.log("PARSER ERROR");
-    console.log(msg);
+    //console.log("PARSER ERROR");
+    //console.log(msg);
     var parser = recognizer._ctx.parser;
     testParser = recognizer._ctx.parser;
     testTokens = parser.getExpectedTokens(); //CRITICAL. This is an interval set with the numbers that correspond to the expected token types
     tokenString = testTokens.toString(); //String like '{1..2, 4..9}' or '8..9'
-     console.log(tokenString);
+     //console.log(tokenString);
      var tokenStrArray = tokenString.replace('{', '').replace('}', '').split(", "); //Array of strings like ['1..2', '4..9']
      var tokenArray = [];
-     console.log(tokenStrArray);
+     //console.log(tokenStrArray);
      for (i = 0; i < tokenStrArray.length; i++) {
          intInterval = tokenStrArray[i];
          if (intInterval.indexOf('..') !== -1) {
@@ -45,13 +46,13 @@ TestGrammarErrorListener.prototype.syntaxError = function(recognizer, offendingS
              tokenArray.push(parseInt(intInterval, 10));
          }
      }
-    console.log(tokenArray);
+    //console.log(tokenArray);
      // End interval set to array of integers conversion
      // Begin convert from array of integers to array of symbolic names
      for (i in tokenArray) {
          tokenArray[i] = parser.symbolicNames[parseInt(tokenArray[i], 10)];
      }
-     console.log(tokenArray);
+     //console.log(tokenArray);
      // End conversion from integers to symbolic names
 
      this.partialApplication = null;
@@ -66,19 +67,13 @@ TestGrammarErrorListener.prototype.syntaxError = function(recognizer, offendingS
         this.tokenType = tokenType;
         if (typeAssistTokens.indexOf(tokenType) >= 0) {
             this.partialApplication = lastToken.text;
-            console.log("partial application: " + this.partialApplication);
+            //console.log("partial application: " + this.partialApplication);
         }
    }
 
 
 
-   var con = mysql.createConnection({
-     host: "localhost",
-     user: "root",
-     password: "root",
-     database: "tokens_db",
-     port: 8889
-   });
+   var con = mysql.createConnection(dbconfig.dbconfig);
 
    var table_name = "";
 
@@ -86,38 +81,39 @@ TestGrammarErrorListener.prototype.syntaxError = function(recognizer, offendingS
 
    con.connect(function(err) {
      if (err) throw err;
-     console.log("Connected!");
+     //console.log("Connected!");
      for (i in tokenArray) {
        table_name = tokenArray[i];
        var sql = "SELECT literal FROM _" + table_name;
        con.query(sql, function (err, result) {
          if (err) throw err;
-         console.log(result[0]["literal"]);
+         //console.log(result[0]["literal"]);
          next_literals.push(result[0]["literal"]);
        });
      }
     setTimeout(function(){
-      console.log("next literals: ", next_literals);
+      //console.log("next literals: ", next_literals);
       var text = "";
       for (i in tokens) {
           if (i < tokens.length - 1) {
              text += tokens[i].text + " ";
           }
       }
-      console.log("text: ", text);
+      //console.log("text: ", text);
       for (i in next_literals) {
         new_str = text + next_literals[i]
         new_str = new_str.trim();
         next_sentences.push(new_str);
       }
-      console.log(next_sentences);
+      //console.log(next_sentences);
+      con.end()
     }, 50);
    });
  }
 
-var antlr4 = require('./antlr4/index.js');
-var NearbyLexer = require('./generated-parser/NearbyLexer.js');
-var NearbyParser = require('./generated-parser/NearbyParser.js');
+var antlr4 = require('antlr4/index');
+var NearbyLexer = require('generated-parser/NearbyLexer');
+var NearbyParser = require('generated-parser/NearbyParser');
 
 function parseStr( my_str ){
     var input = my_str;
@@ -149,7 +145,7 @@ function showResult(str) {
     parser.removeErrorListeners();
     parser.addErrorListener(listener);
     parser.buildParseTrees = true;
-    console.log(parser._errHandler); //TEST
+    //console.log(parser._errHandler); //TEST
     var tree = parser.application();
 
     var tokens = parser.getTokenStream().tokens;
@@ -164,60 +160,65 @@ function showResult(str) {
 
 function processPartialStrs() {
   if (partial_strs.length != 0) {
-    console.log("processing")
+    //console.log("processing")
     parse_succeeded = true;
     next_sentences = new Array();
     test_str = partial_strs.pop().toLowerCase();
     parseStr(test_str);
-    console.log("post parse success", parse_succeeded)
-    console.log("next sentences", next_sentences);
+    //console.log("post parse success", parse_succeeded)
+    //console.log("next sentences", next_sentences);
     if (!parse_succeeded) {
-      console.log(test_str);
+      //console.log(test_str);
       setTimeout(function(){
         for (i = 0; i < next_sentences.length; i++) {
           var next_sentence = next_sentences[i];
           partial_strs.push(next_sentence);
         }
-        console.log(partial_strs);
-        console.log(partial_strs.length);
+        //console.log(partial_strs);
+        //console.log(partial_strs.length);
         processPartialStrs();
       }, 100);
     } else {
-      console.log("more processing");
+      //console.log("more processing");
       language.push(test_str);
       processPartialStrs();
     }
   } else {
-    console.log("continuing...");
-    console.log(language);
-    /*
-    var con = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "root",
-      database: "tokens_db"
-    });
+    //console.log("continuing...");
+    //console.log(language);
 
+    var con = mysql.createConnection(dbconfig.dbconfig);
+
+    var tablename = "language";
 
     con.connect(function(err) {
       if (err) throw err;
-      console.log("Connected!");
-      var sql = "DROP TABLE IF EXISTS customers";
-      con.query(sql, function (err, result) {
+      console.log("Connected to database...");
+      // delete language table if exists
+      var drop_table = "DROP TABLE IF EXISTS " + tablename;
+      con.query(drop_table, function (err, result) {
         if (err) throw err;
-        console.log(result);
-      });*/
-      /*var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
+        // create new language table
+        var create_table = "CREATE TABLE " + tablename + " (sentence VARCHAR(255))"
+        con.query(create_table, function (err, result) {
+          if (err) throw err;
+          // insert new language (insert into for each sentence)
+          var insert = "INSERT INTO " + tablename + " (sentence) VALUES ?";
+          var values = [];
+          for (i in language) {
+            values.push([language[i]]);
+          }
+          //var values = language;
+          con.query(insert, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+            con.end()
+            process.exit();
+          });
+        });
       });
-      sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-      });*/
-    //});
+    });
+
   }
 }
 
