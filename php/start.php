565e7aede099a9a9f7051145cfc141127a1dd497
Meghan <meghan.leah.clark@gmail.com>
2018-08-27 15:53:15 -0700

    searches for sentences in the language that start with the text

---
+++ php/start.php
@@ -0,0 +1,31 @@
+<?php
+include "database-credentials.php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//$mytable = $_GET["q"];
+$partialtext = $_GET["text"];
+
+//for($i=0; $i<count($mytable); $i++) {
+$sql = "SELECT sentence FROM language WHERE sentence LIKE '" . $partialtext . "%'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $i = 0;
+    while($row = $result->fetch_assoc()) {
+        //$suggestion = $row["sentence"] . "..." . "<br>";
+        $suggestion = "<div onmouseover=\"highlight_suggestion(this);\" onmouseout=\"unhighlight_suggestion(this);\" onclick=\"autofill(this);\" class=\"suggestion\" id=\"suggestion" . $i . "\">" . ucfirst($row["sentence"]) . "</div>";
+        $i++;
+        echo ucfirst($suggestion);
+    }
+} //else {
+//    echo "0 results";
+
+$conn->close();
+?>
